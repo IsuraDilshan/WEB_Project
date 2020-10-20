4dc155c95a4c95a7648bbfc8fd9f62307b1ba266
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2020-10-21 02:36:11 +0530

    updated login without host

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+$message="";
+if(count($_POST)>0) {
+	$conn = mysqli_connect("localhost","root","","phppot_examples");
+	$result = mysqli_query($conn,"SELECT * FROM users WHERE user_name='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+	$count  = mysqli_num_rows($result);
+	if($count==0) {
+		$message = "Invalid Username or Password!";
+	} else {
+		$message = "You are successfully authenticated!";
+	}
+}
+?>
+<html>
+<head>
+<title>User Login</title>
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+	<div class="message"><?php if($message!="") { echo $message; } ?></div>
+		<table border="0" cellpadding="10" cellspacing="1" width="500" align="center" class="tblLogin">
+			<tr class="tableheader">
+			<td align="center" colspan="2">Enter Login Details</td>
+			</tr>
+			<tr class="tablerow">
+			<td>
+			<input type="text" name="userName" placeholder="User Name" class="login-input"></td>
+			</tr>
+			<tr class="tablerow">
+			<td>
+			<input type="password" name="password" placeholder="Password" class="login-input"></td>
+			</tr>
+			<tr class="tableheader">
+			<td align="center" colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
+			</tr>
+		</table>
+</form>
+<center>
+	<div id="bDiv">
+		<lable id="lbl">Have not an account?</lable><br><br>
+		<button class="btn">I'm a tourist</button>
+		<button class="btn" style="margin-left:20px;margin-right:20px;">I'm a guide</button>
+		<button class="btn">I'm a driver</button>
+	</div>
+</center>
+</body></html>
