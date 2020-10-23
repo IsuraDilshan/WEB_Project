da7ed777140e30ab9edfe56c3571e5de1d87120e
shamilkeheliya <shamilkeheliya@users.noreply.github.com>
2020-10-23 23:40:22 +0530

    Updated logings

---
+++ php/tourist_logged.php
@@ -0,0 +1,11 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["tourist"]) || $_SESSION["tourist"] !== true){
+    header("location: login.php");
+    exit;
+}
+$uname = $_SESSION["uname"];
+?>
